7d38a54563ddddfb7e9a848fe8b07f7d68f16993
ridhodaffasyah <ridhodaffasyah01@gmail.com>
2024-03-03 14:01:53 +0700

    fix: clicked area button card and trash

--- src/components/atom/Card/index.tsx
+++ src/components/atom/Card/index.tsx
@@ -9,7 +9,7 @@ const Card = ({ id, title, date, onModal }: CardProps) => {
       className="bg-white hover:bg-slate-100 hover:cursor-pointer w-full flex justify-between sm:w-[49.2%] lg:w-[23.88%] shadow-md drop-shadow rounded-xl"
     >
       <div
-        className="flex flex-col justify-between p-5 gap-20"
+        className="flex flex-col justify-between p-5 gap-20 w-full"
         onClick={() => navigate(`/detail/${id}`)}
       >
         <h1 className="font-bold text-[18px]" data-cy="activity-item-title">
@@ -20,7 +20,7 @@ const Card = ({ id, title, date, onModal }: CardProps) => {
         </div>
       </div>
 
-      <div className="flex justify-end w-[12%] items-end pb-6 mr-3">
+      <div className="flex justify-center w-[10%] absolute right-3 bottom-5">
         <button onClick={onModal}>
           <img
             src="/assets/tabler_trash.png"
